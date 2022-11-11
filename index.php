eae671c271b3b472c3b5616cb218f6836b6e684a
DakhwazHassan <116338423+DakhwazHassan@users.noreply.github.com>
2022-11-11 11:20:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
 	<body>
 		<h1> Das ist der Titel<h1>
 		<p>Hallo Welt leyla! </p>
-		<img src="https://cdn.shopify.com/s/files/1/0266/5817/7121/files/image2.jpg?v=1646720071" alt="Kuhle Ente">
 		<img scr="https://cdns-images.dzcdn.net/images/artist/4876babd658a69344e0264c2e40f6bcd/500x500.jpg" alt="SSio">
 		<p> HAb es geschafft</p>
 		<p> Ceaband, ist ein gangster</p>
