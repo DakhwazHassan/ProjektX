3ac83b7d9e548e2409b3dac1834234c012a1c652
A-D-Sio <117442472+A-D-Sio@users.noreply.github.com>
2022-11-11 11:23:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,14 @@
 		<h1> Das ist der Titel</h1>
 		<meta charset="UTF-8">
 		<title></title>
+		<img src="https://cdn.shopify.com/s/files/1/0266/5817/7121/files/image2.jpg?v=1646720071" alt="Kuhle Ente">
+		<p> HAb es geschafft</p>
 	<head>
 	<body>
 		<h1> Das ist der Titel<h1>
 		<p>Hallo Welt leyla! </p>
 		<img scr="https://upload.wikimedia.org/wikipedia/commons/8/8e/Deichbrand2018-SSIO-28.jpg" alt="SSio">
-		<p> HAb es geschafft</p>
-		<p> Ceaband, ist ein gangster</p>
+		<p> Hab es geschafft</p>
+		<p> Dakhwaz stinkt!</p>
 	<body>
 <html>
