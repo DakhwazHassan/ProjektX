d08d98134c5bc3f886e5fb1ea07878f662d247b9
DakhwazHassan <116338423+DakhwazHassan@users.noreply.github.com>
2022-11-11 11:21:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<body>
 		<h1> Das ist der Titel<h1>
 		<p>Hallo Welt leyla! </p>
-		<img scr="https://cdns-images.dzcdn.net/images/artist/4876babd658a69344e0264c2e40f6bcd/500x500.jpg" alt="SSio">
+		<img scr="https://upload.wikimedia.org/wikipedia/commons/8/8e/Deichbrand2018-SSIO-28.jpg" alt="SSio">
 		<p> HAb es geschafft</p>
 		<p> Ceaband, ist ein gangster</p>
 	<body>
