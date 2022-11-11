a9a85ddb799887f06eed9a0dbb000876348462d7
DakhwazHassan <116338423+DakhwazHassan@users.noreply.github.com>
2022-11-11 11:14:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <html>
 	<head>
+		<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.deezer.com%2Fde%2Fartist%2F5095703&psig=AOvVaw0kTSJLGGLbuBJzwf09-ENM&ust=1668248056030000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCKDA5LfypfsCFQAAAAAdAAAAABAE"> 
 		<h1> Das ist der Titel</h1>
 		<meta charset="UTF-8">
 		<title></title>
