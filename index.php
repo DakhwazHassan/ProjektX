921e4cce422a3a9dad5211b65e822536fa591505
DakhwazHassan <116338423+DakhwazHassan@users.noreply.github.com>
2022-11-25 11:39:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<meta charset="UTF-8">
 		<title></title>
 		<img src="https://hips.hearstapps.com/hmg-prod/images/wrinkle-the-duck-running-nyc-marathon-social-1636731968.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*" alt="Kuhle Ente">
-		<p> HAb es geschafft</p>
+		
 	<head>
 	<body>
 		
@@ -16,7 +16,36 @@
 			<a href="ssioder@mother.fucker">Email senden</a>
 		<img scr="https://upload.wikimedia.org/wikipedia/commons/8/8e/Deichbrand2018-SSIO-28.jpg" alt="SSio">
 		<article>
-	
+			<header class="Kopffluss">kopfzeile</header>
+			<nac class0"bigfront">Navigation</nav>
+			<main>
+			<body>
+				<h1>Tabelle ohne name</h1>
+				<table>
+					<thead>
+					  <tr>
+					    <th>Spalte 1</th>
+					    <th>Spalte 2</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Kiste 1.1</td>
+							<td>Kiste 2.1</td>
+						</tr>
+						<tr>
+							<td>Kiste 1.2</td>
+							<td>Kiste 2.2</td>
+						</tr>
+						<tr>
+							<td>Kiste 1.3</td>
+							<td>Kiste 2.3</td>
+						</tr>
+					</tbody>
+					
+			
+			
+			</main>
 		
 		<p> Ceaband, ist ein gangster</p>
 		<p> hab ich das jetzt geschafft </p>
