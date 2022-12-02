a5bf11aeac0586e598aeee7a4dc83e469e12a139
A-D-Sio <117442472+A-D-Sio@users.noreply.github.com>
2022-12-02 10:40:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
 <html>
 	<head>
-		
-		<h1> Kanax in Kitchen 2</h1>
 		<meta charset="UTF-8">
 		<title></title>
 		<img src="https://hips.hearstapps.com/hmg-prod/images/wrinkle-the-duck-running-nyc-marathon-social-1636731968.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*" alt="Kuhle Ente">
@@ -12,7 +10,6 @@
 	<body>
 		
 		<h1> Das ist der Titel<h1>
-		<p>Hallo Welt leyla! </p>
 			<a href="ssioder@mother.fucker">Email senden</a>
 		<img scr="https://upload.wikimedia.org/wikipedia/commons/8/8e/Deichbrand2018-SSIO-28.jpg" alt="SSio">
 		<article>
